5fcb0f00d9fa4b630ed2a605795279dde09f9c49
Shuichiro Kudaka <e125750@ie.u-ryukyu.ac.jp>
2017-07-31 13:57:20 +0900

    add Tapped event

--- KabeDon/MainPage.xaml.cs
+++ KabeDon/MainPage.xaml.cs
@@ -5,12 +5,14 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
@@ -25,6 +27,22 @@ namespace KabeDon
         public MainPage()
         {
             this.InitializeComponent();
+            image.Tapped += Image_Tapped;
+        }
+
+        private void Image_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var imageUri = new Uri("ms-appx:///Assets/Image/Claudia2.png");
+            ShowImage(imageUri);
+        }
+
+        private async void ShowImage(Uri imageUri)
+        {
+            var bitmap = new BitmapImage();
+            var file = await StorageFile.GetFileFromApplicationUriAsync(imageUri);
+            var stream = await file.OpenReadAsync();
+            await bitmap.SetSourceAsync(stream);
+            image.Source = bitmap;
         }
     }
 }
